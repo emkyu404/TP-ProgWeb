85b21473e4a220b434384d07887bc892dbd1ae70
Emkyu404 <contact.buiminhquan@gmail.com>
2020-11-30 15:45:39 +0100

    tp4

---
+++ TP4/client/components/AddArticle.vue
@@ -0,0 +1,36 @@
+<template>
+    <form @submit.prevent="addArticle" v-show="show">
+      <h2>Nouveau produit à ajouter</h2>
+      <input type="text" v-model="newArticle.name" placeholder="Nom du produit" required>
+      <input type="number" v-model="newArticle.price" placeholder="Prix" required>
+      <textarea type="text" v-model="newArticle.description" required></textarea>
+      <input type="text" v-model="newArticle.image" placeholder="Lien vers l'image">
+      <button type="submit">Ajouter</button>
+    </form>
+</template>
+ 
+<script>
+module.exports = {
+  data () {
+    return {
+        newArticle: {
+        name: '',
+        description: '',
+        image: '',
+        price: 0
+      }
+    }
+  },
+  methods: {
+      addArticle () {
+      this.$emit('add-article', this.newArticle)
+    }
+  },
+  props:{
+      show: Boolean
+  }
+}
+</script>
+ 
+<style>
+</style>
